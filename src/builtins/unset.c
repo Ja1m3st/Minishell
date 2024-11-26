22aa9bbeac5b0edb4d9663fdda7cb95de6bc14f2
Jaime Sanchez <jaimesan@c1r6s3.42madrid.com>
2024-11-26 13:39:56 +0100

    f

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,11 +6,29 @@
 /*   By: jaimesan <jaimesan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:54:29 by jaimesan          #+#    #+#             */
-/*   Updated: 2024/11/26 13:11:47 by jaimesan         ###   ########.fr       */
+/*   Updated: 2024/11/26 13:38:06 by jaimesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*find_env_variable(char **env, const char *key)
+{
+	int	i;
+	int	key_len;
+
+	i = 0;
+	if (!env || !key)
+		return (NULL);
+	key_len = ft_strlen(key);
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], key, key_len) == 0 && env[i][key_len] == '=')
+			return (env[i]);
+		i++;
+	}
+	return (NULL);
+}
 
 void	unset(t_mini *mini)
 {
@@ -20,28 +38,24 @@ void	unset(t_mini *mini)
 	int		j;
 	int		cmd_len;
 
-	if (!mini->cmds[1])
-		return ;
-	if (!getenv(mini->cmds[1]))
+	if (!mini->cmds[1] || find_env_variable(mini->env, mini->cmds[1]) == NULL)
 		return ;
 	len = array_len(mini->env);
 	cmd_len = ft_strlen(mini->cmds[1]);
-
 	new_env = malloc(len * sizeof(char *));
 	if (!new_env)
 		error(mini, 'M');
-
 	i = 0;
 	j = 0;
 	while (i < len)
 	{
-		if (ft_strncmp(mini->env[i], mini->cmds[1], cmd_len) == 0 && mini->env[i][cmd_len] == '=')
+		if (ft_strncmp(mini->env[i], mini->cmds[1], cmd_len) == 0
+			&& mini->env[i][cmd_len] == '=')
 		{
 			i++;
 			continue ;
 		}
-		new_env[j++] = ft_strdup(mini->env[i]);
-		i++;
+		new_env[j++] = ft_strdup(mini->env[i++]);
 	}
 	new_env[j] = NULL;
 	free_arr(mini->env);

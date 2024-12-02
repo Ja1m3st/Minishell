d1f30ffdad958f37f96e91dcb2247f839a611547
Jaime Sanchez <jaimesan@c2r4s6.42madrid.com>
2024-12-02 15:17:48 +0100

    d

--- src/main/get_commands.c
+++ src/main/get_commands.c
@@ -6,13 +6,13 @@
 /*   By: jaimesan <jaimesan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:30:27 by jaimesan          #+#    #+#             */
-/*   Updated: 2024/12/02 15:12:44 by jaimesan         ###   ########.fr       */
+/*   Updated: 2024/12/02 15:17:38 by jaimesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	built_int(t_mini *mini)
+void	get_commands(t_mini *mini)
 {
 	save_cmds(mini);
 	if (mini->total_args == 1)
@@ -25,7 +25,7 @@ void	built_int(t_mini *mini)
 		else if (!ft_strcmp(mini->cmds[0], "echo"))
 			echo(mini);
 		else if (!ft_strcmp(mini->cmds[0], "pwd"))
-			print_pwd(mini);
+			print_pwd();
 		else if (!ft_strcmp(mini->cmds[0], "cd"))
 			cd(mini);
 		else if (!ft_strcmp(mini->cmds[0], "export"))
@@ -38,16 +38,15 @@ void	built_int(t_mini *mini)
 				printf("%s\n", get_var(mini));
 		}
 		else if (!ft_strcmp(mini->cmds[0], "exit"))
-		{
-			write(1, "exit\n", 5);
-			exit(EXIT_SUCCESS);
-		}
+			error(mini, '!');
 		else
 			get_terminal_commands(mini);
+		free_arr(mini->cmds);
 	}
 	else
 		pipex(mini);
 }
+
 
 
 void	get_commands(t_mini *mini)

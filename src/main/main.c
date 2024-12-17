0513407dc2ea84bad5101f0f71d4f725e8f1ca8a
Cristopher Tommasi <ctommasi@student.42madrid.com>
2024-12-17 15:25:37 +0100

    hoa

--- src/main/main.c
+++ src/main/main.c
@@ -39,6 +39,7 @@ int	main(int argc, char **argv, char **env)
 		// if (check_quotation(&mini, i) == 0)
 		// 	continue ;
 		tokenize_commands(&mini, mini.mini_cmds);
+		print_tree_structure(&mini);
 		execute_commands(&mini);
 		free_main(&mini);
 	}
--- src/utils/here_doc.c
+++ src/utils/here_doc.c
@@ -38,3 +38,30 @@ void	here_doc(t_mini *mini, t_token *token)
 	if (dup2(fd[0], mini->infile) == -1)
 		return (perror("Here Doc Error.\n"), exit(EXIT_FAILURE));
 }
+
+// void	here_doc(t_mini *mini, t_token *token)
+// {
+// 	char	*line;
+// 	int		fd[2];
+
+// 	if (pipe(fd) == -1)
+// 		return (perror("Pipe Error\n"));
+// 	while (1)
+// 	{
+// 		write(1, "> ", 2);
+// 		line = get_next_line(0);
+// 		if (!line)
+// 			break ;
+// 		if (!ft_strncmp(line, token->delimeter, ft_strlen(token->delimeter))
+// 			&& line[ft_strlen(token->delimeter)] == '\n')
+// 		{
+// 			free(line);
+// 			break ;
+// 		}
+// 		write(fd[1], line, ft_strlen(line));
+// 		free(line);
+// 	}
+// 	close(fd[1]);
+// 	if (dup2(fd[0], mini->infile) == -1)
+// 		return (perror("Here Doc Error.\n"), exit(EXIT_FAILURE));
+// }
